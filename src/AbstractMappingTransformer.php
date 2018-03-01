c88662c006cb5bb9830269c17b83f5afa7bf864c
Donovan Bourlard <donovan.bourlard@biig.fr>
2018-03-01 13:45:58 +0100

    Enable condition key node

--- src/AbstractMappingTransformer.php
+++ src/AbstractMappingTransformer.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Valouleloup\ArrayMapping;
 
 use Symfony\Component\PropertyAccess\PropertyAccess;
@@ -20,103 +18,143 @@ abstract class AbstractMappingTransformer
     }
 
     /**
-     * Parse input array to output array according to a YAML config file
+     * Transform the array given in parameter to an output array
+     * Method goal:
+     * - Load transformer config (yaml file)
+     * - Transform input array to output formatted array
      *
-     * @param array $data The input array to transform
-     * @return array The output array (input array transformed)
+     * @param array $data
+     * @return mixed
      */
     abstract protected function transform(array $data);
 
     /**
-     * @param array $mapping
-     * @param array $data
-     * @return array
+     * Generate output array by given mapping & input array
+     *
+     * @param array $mapping The mapping to use to transform input array
+     * @param array $data The input array
+     * @return array The output array
      * @throws \Exception
      */
     protected function transformFromMapping(array $mapping, array $data)
     {
         $result = [];
 
-        foreach ($mapping as $key) {
-            $keyValue = $this->getKeyValue($key, $data);
+        foreach ($mapping as $node) {
+            // Only transform current node if no condition prevent it
+            if ($this->areConditionsValidated($node, $data)) {
+                // Get our node value
+                $nodeValue = $this->getNodeValue($node, $data);
 
-            if (isset($key['required']) && true === $key['required'] && null === $keyValue) {
-                throw new \Exception('Field ' . $key['from'] . ' required.');
-            }
+                // If we didn't get value but field was required, throw Exception
+                if (isset($node['required']) && true === $node['required'] && null === $nodeValue) {
+                    throw new \Exception('Field ' . $node['from'] . ' required.');
+                }
 
-            if (isset($key['dependencies'])) {
-                $dependenciesExist = true;
+                if (isset($node['dependencies'])) {
+                    $dependenciesExist = true;
 
-                foreach ($key['dependencies'] as $dependency) {
-                    if (null === $this->getValue($result, $mapping[$dependency]['to'])) {
-                        $dependenciesExist = false;
+                    foreach ($node['dependencies'] as $dependency) {
+                        if (null === $this->getValue($result, $mapping[$dependency]['to'])) {
+                            $dependenciesExist = false;
+                        }
+                    }
+
+                    if ($dependenciesExist && null === $nodeValue) {
+                        throw new \Exception('Field ' . $node['from'] . ' required if dependencies true.');
                     }
                 }
 
-                if ($dependenciesExist && null === $keyValue) {
-                    throw new \Exception('Field ' . $key['from'] . ' required if dependencies true.');
+                // nodeValue was found && acceptable, add value to result array
+                if (null !== $nodeValue) {
+                    $this->accessor->setValue($result, $this->convertToBrackets($node['to']), $nodeValue);
                 }
-            }
-
-            if (null !== $keyValue) {
-                $this->accessor->setValue($result, $this->convertToBrackets($key['to']), $keyValue);
             }
         }
 
         return $result;
     }
+    
+    /**
+     * Check if a node can be transformed
+     *
+     * @param array $node The node to check
+     * @param array $data The input date to parse
+     * @return bool True if node is transformable, False if node isn't transformable
+     */
+    private function areConditionsValidated(array $node, array $data)
+    {
+        // If node doesn't have condition, execute transformation
+        if (!isset($node['condition'])) {
+            return true;
+        }
+
+        // Only transform node if field exists in array to parse
+        if (isset($node['condition']['exists'])
+            && \is_null($this->getValue($data, $node['condition']['exists']))) {
+            return false;
+        }
+
+        return true;
+    }
 
     /**
-     * @param array $key
+     * Get the value related to a node
+     *
+     * @param array $node
      * @param array $data
      *
      * @return mixed
      */
-    private function getKeyValue(array $key, array $data)
+    private function getNodeValue(array $node, array $data)
     {
         //1. Get function value if is set so
         //2. Else get raw value is no function is defined
         //3. If keyValue is null and a default value is set, return default value
-        $keyValue = null;
+        $nodeValue = null;
 
-        if (isset($key['function'])) {
+        if (isset($node['function'])) {
             $params = [];
 
-            foreach ($key['function']['params'] as $param) {
+            foreach ($node['function']['params'] as $param) {
                 $params[] = $this->getValue($data, $param);
             }
 
-            $keyValue = call_user_func_array([$this, $key['function']['name']], $params);
+            $nodeValue = call_user_func_array([$this, $node['function']['name']], $params);
         } else {
-            if (isset($key['from'])) {
-                $keyValue = $this->getValue($data, $key['from']);
+            if (isset($node['from'])) {
+                $nodeValue = $this->getValue($data, $node['from']);
             }
         }
 
-        if (null === $keyValue && isset($key['default'])) {
-            $keyValue = $key['default'];
+        if (null === $nodeValue && isset($node['default'])) {
+            $nodeValue = $node['default'];
         }
 
-        return $keyValue;
+        return $nodeValue;
     }
 
     /**
-     * @param array $data
-     * @param string $key
+     * Get value set in the input array by finding them with key
      *
-     * @return mixed
+     * @param array $data The input array
+     * @param string $key The key which is the location of the value in the input array
+     *
+     * @return mixed|null The related value, if value doesn't exists return null
      */
-    private function getValue(array $data, string $key)
+    private function getValue(array $data, $key)
     {
         return $this->accessor->getValue($data, $this->convertToBrackets($key));
     }
 
     /**
-     * @param string $path
+     * Convert path separated with dot to path separated with brackets
+     * ie. path.to.convert will produce [path][to][convert]
      *
-     * @return string
+     * @param string $path The path to convert
+     * @return string the converted path
      */
-    private function convertToBrackets(string $path)
+    private function convertToBrackets($path)
     {
         $keys = explode('.', $path);
 
